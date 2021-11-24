f423f2aa88ef0a54684e3b477984147f53f8eb8d
GreenbullGit <green@DESKTOP-O91PG8I.localdomain>
2021-11-24 01:12:48 +0100

    new

--- src/Home.tsx
+++ src/Home.tsx
@@ -27,7 +27,7 @@ const ConnectButton = styled(WalletDialogButton)({
   borderRadius: 4,
 });
 
-const CounterText = styled.span``; // add your styles heree
+const CounterText = styled.span``; // add your styles here
 
 const MintContainer = styled.div``; // add your styles here
 
